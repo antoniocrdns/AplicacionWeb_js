b9895c6af4cc6816ad5e0ae07205ad59c7721468
antoniocrdns <gohan21.d@gmail.com>
2023-01-16 08:07:01 -0700

    Se realizo ejercicio del forEach imprimiendo una cadena de objetos

---
+++ forEach.js
@@ -0,0 +1,28 @@
+const edades = [22, 4, 5, 8, 9];
+
+/*edades.forEach((valor) => {
+    console.log(valor);
+});*/
+
+const usuario = [
+    {
+        nombre: 'Antonio',
+        dinero: false,
+        edad: '20'
+    },
+    {
+        nombre: 'Victor',
+        dinero: true,
+        edad: '31'
+    },
+    {
+        nombre: 'Alka',
+        dinero: true,
+        edad: '25 + 3 DLC'
+    }
+];
+
+usuario.forEach((valor) => {
+    console.log(valor);
+});
+
