c6e678df3d174e5b0f1bb3334f2160644a6a429d
antoniocrdns <gohan21.d@gmail.com>
2023-01-23 08:44:27 -0700

    Se realizo un ejercicio en Vue con model

---
+++ Vue/model.js
@@ -0,0 +1,24 @@
+Vue.createApp({
+    data(){
+        return{
+            edad: 0
+        }
+    },
+    methods: {
+        capturarEdad() {
+            //this.edad = prompt("Dame tu edad: ");
+            if (this.edad >= 18 && this.edad <= 100) {
+                console.log("Pasale papu al desmadre ya estas bien ruco");
+            }
+            else if (this.edad > 100) {
+                console.log("Ya deberias estar muerto");
+            }
+            else if (this.edad <= 0) {
+                console.log("Eso no existe");
+            }
+            else {
+                console.log("No prro tas muy chavo");
+            }
+        }
+    },
+}).mount('#app')
